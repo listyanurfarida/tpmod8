3e34c74b831d67db6afc0f326d0e654aa14e92f3
ainunfauziyahbahary <ainunfauziyahbahary@gmail.com>
2015-04-07 06:34:44 +0700

    new commit

---
+++ src/helloword/Helloword.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloword;
+
+/**
+ *
+ * @author Ainun
+ */
+public class Helloword {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, saya Listya. NIM saya 6306134097. Saya sangat menyukai praktikum modul 8 ini.");
+    }
+    
+}
